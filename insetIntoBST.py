4dbca0c3ea609e9125cd7f285e84be6917cca1af
Neil Bajaj <104532169+nb110240@users.noreply.github.com>
2022-08-02 15:14:41 -0700

    Add files via upload

---
+++ insetIntoBST.py
@@ -0,0 +1,8 @@
+def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if root == None:
+            return TreeNode(val)
+        if val > root.val: 
+            root.right = self.insertIntoBST(root.right,val)
+        elif val < root.val: 
+            root.left = self.insertIntoBST(root.left,val)
+        return root
