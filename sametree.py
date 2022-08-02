fc8e3f6e6f0869af3e079a5f812ff8c9e326ea2b
Neil Bajaj <104532169+nb110240@users.noreply.github.com>
2022-08-02 15:14:28 -0700

    Add files via upload

---
+++ sametree.py
@@ -0,0 +1,8 @@
+def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not q or not p:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.right, q.right) and self.isSameTree( p.left, q.left)
