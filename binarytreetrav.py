b41b089013e4b9363e08c150c69d33c2e78fca66
Neil Bajaj <104532169+nb110240@users.noreply.github.com>
2022-08-03 17:55:08 -0700

    Add files via upload

---
+++ binarytreetrav.py
@@ -0,0 +1,18 @@
+#level order traversal 
+def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        q = collections.deque()
+        res = []
+        q.append(root)
+        
+        while q: 
+            qLen = len(q)
+            level = []
+            for i in range(qLen):
+                node = q.popleft()
+                if node: 
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+        return res
